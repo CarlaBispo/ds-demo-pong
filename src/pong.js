bce499a7a209f4d8786ed7bd7423d37c423a783a
Gary Burnett <garyburnett3000@gmail.com>
2015-07-01 22:41:58 +0100

    Move data from pong to modules

--- src/pong.js
+++ src/pong.js
@@ -2,55 +2,20 @@
 // PONG
 //=============================================================================
 var Game = require('./game.js'),
+    defaults = require('./pongDefaults.js'),
+    colors = require('./pongColors.js'),
+    images = require('./pongImages.js'),
+    ai = require('./pongAI.js'),
+    courtDigits = require('./pongDigits.js'),
     Pong = {
 
-        Defaults: {
-            width:        640,   // logical canvas width (browser will scale to physical canvas size - which is controlled by @media css queries)
-            height:       480,   // logical canvas height (ditto)
-            wallWidth:    12,
-            paddleWidth:  12,
-            paddleHeight: 60,
-            paddleSpeed:  2,     // should be able to cross court vertically   in 2 seconds
-            ballSpeed:    4,     // should be able to cross court horizontally in 4 seconds, at starting speed ...
-            ballAccel:    8,     // ... but accelerate as time passes
-            ballRadius:   5,
-            sound:        true
-        },
+        Defaults: defaults,
 
-        Colors: {
-            walls:           'white',
-            ball:            'white',
-            score:           'white',
-            footprint:       '#333',
-            predictionGuess: 'yellow',
-            predictionExact: 'red'
-        },
+        Colors: colors,
 
-        Images: [
-            "images/press1.png",
-            "images/press2.png",
-            "images/winner.png"
-        ],
+        Images: images,
 
-        Levels: [
-            {aiReaction: 0.2, aiError:  40}, // 0:  ai is losing by 8
-            {aiReaction: 0.3, aiError:  50}, // 1:  ai is losing by 7
-            {aiReaction: 0.4, aiError:  60}, // 2:  ai is losing by 6
-            {aiReaction: 0.5, aiError:  70}, // 3:  ai is losing by 5
-            {aiReaction: 0.6, aiError:  80}, // 4:  ai is losing by 4
-            {aiReaction: 0.7, aiError:  90}, // 5:  ai is losing by 3
-            {aiReaction: 0.8, aiError: 100}, // 6:  ai is losing by 2
-            {aiReaction: 0.9, aiError: 110}, // 7:  ai is losing by 1
-            {aiReaction: 1.0, aiError: 120}, // 8:  tie
-            {aiReaction: 1.1, aiError: 130}, // 9:  ai is winning by 1
-            {aiReaction: 1.2, aiError: 140}, // 10: ai is winning by 2
-            {aiReaction: 1.3, aiError: 150}, // 11: ai is winning by 3
-            {aiReaction: 1.4, aiError: 160}, // 12: ai is winning by 4
-            {aiReaction: 1.5, aiError: 170}, // 13: ai is winning by 5
-            {aiReaction: 1.6, aiError: 180}, // 14: ai is winning by 6
-            {aiReaction: 1.7, aiError: 190}, // 15: ai is winning by 7
-            {aiReaction: 1.8, aiError: 200}  // 16: ai is winning by 8
-        ],
+        Levels: ai,
 
         //-----------------------------------------------------------------------------
 
@@ -294,18 +259,7 @@ var Game = require('./game.js'),
                     ctx.fillRect(x, y+h-dh, w, dh);
             },
 
-            DIGITS: [
-                [1, 1, 1, 0, 1, 1, 1], // 0
-                [0, 0, 1, 0, 0, 1, 0], // 1
-                [1, 0, 1, 1, 1, 0, 1], // 2
-                [1, 0, 1, 1, 0, 1, 1], // 3
-                [0, 1, 1, 1, 0, 1, 0], // 4
-                [1, 1, 0, 1, 0, 1, 1], // 5
-                [1, 1, 0, 1, 1, 1, 1], // 6
-                [1, 0, 1, 0, 0, 1, 0], // 7
-                [1, 1, 1, 1, 1, 1, 1], // 8
-                [1, 1, 1, 1, 0, 1, 0]  // 9
-            ]
+            DIGITS: courtDigits
 
         },
 
---
+++ src/pongImages.js
@@ -0,0 +1,5 @@
+module.exports = [
+    "images/press1.png",
+    "images/press2.png",
+    "images/winner.png"
+];
